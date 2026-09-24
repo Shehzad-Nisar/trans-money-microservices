6a474e8a635fca1668f1d6c9431434c2edc86262
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-09-24 23:08:47 +0500

    dto/response

--- account-service/src/main/java/com/transmoney/accountservice/dto/AccountResponse.java
+++ account-service/src/main/java/com/transmoney/accountservice/dto/AccountResponse.java
@@ -1,5 +1,21 @@
 package com.transmoney.accountservice.dto;
 
+import com.transmoney.accountservice.Entity.AccountStatus;
+import com.transmoney.accountservice.Entity.AccountType;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class AccountResponse {
-    
+
+    private String id;
+    private String accountNumber ;
+    private String accountHolderName;
+    private String email;
+    private String phone;
+    private AccountType accountType;
+    private AccountStatus accountStatus;
+    private BigDecimal balance;
+    private BigDecimal dailyTransactionLimit;
+    private LocalDateTime createdAt;
+
 }
