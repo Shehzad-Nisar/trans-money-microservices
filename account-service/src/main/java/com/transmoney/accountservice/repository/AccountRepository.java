bd8d80167c10009461cfd22d497d1f219b671c5e
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-09-25 12:23:47 +0500

    define bussiness logics

--- account-service/src/main/java/com/transmoney/accountservice/repository/AccountRepository.java
+++ account-service/src/main/java/com/transmoney/accountservice/repository/AccountRepository.java
@@ -1,7 +1,13 @@
 package com.transmoney.accountservice.repository;
 
 import com.transmoney.accountservice.Entity.Account;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account,String> {
+
+    boolean existsByEmail(@NotBlank(message = "Account holder's email is required.") @Email(message = "Invalid email format.") String email);
+
 }
+
--- account-service/src/main/java/com/transmoney/accountservice/service/AccountService.java
+++ account-service/src/main/java/com/transmoney/accountservice/service/AccountService.java
@@ -1,4 +1,35 @@
 package com.transmoney.accountservice.service;
 
+import com.transmoney.accountservice.Entity.Account;
+import com.transmoney.accountservice.Entity.AccountStatus;
+import com.transmoney.accountservice.dto.AccountResponse;
+import com.transmoney.accountservice.dto.CreateAccountRequest;
+import com.transmoney.accountservice.repository.AccountRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+
 public class AccountService {
+    AccountRepository accountRepository;
+    public AccountResponse createAccount(CreateAccountRequest request) {
+        log.info("Creating an account for : {} " + request.getEmail());
+
+        if(accountRepository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("Account already exited against this email: " + request.getEmail());
+        }
+
+        Account account = new Account();
+        account.setAccountHolderName(request.getAccountHolderName());
+        account.setEmail(request.getEmail());
+        account.setPhone(request.getPhone());
+        account.setAccountType(request.getAccountType());
+        account.setAccountStatus(AccountStatus.ACTIVE);
+        account.setBalance(request.getInitialDeposit());
+
+
+    }
 }
